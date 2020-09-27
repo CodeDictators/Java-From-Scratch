c5659c5c689cc11e364c2b8dc7724ecafc784770
kalpesh-mali <kalpesh.mali@bridgelabz.com>
2020-09-28 03:05:10 +0530

    HelloWorld Program with Disc

---
+++ HelloWorld.java
@@ -0,0 +1,26 @@
+package com.codedictator.learning;
+
+//Package in Java is a mechanism to encapsulate a group of classes, sub packages and interfaces.
+
+//public keyword is an access modifier which represents visibility. It means it is visible to all.
+//class keyword is used to declare a class in java.
+//static is a keyword. If we declare any method as static, it is known as the static method. The core advantage of the static method is that there is no need to create an object to invoke the static method. The main method is executed by the JVM, so it doesn't require to create an object to invoke the main method. So it saves memory.
+//void is the return type of the method. It means it doesn't return any value.
+//main represents the starting point of the program.
+//String[] args is used for command line argument.
+//System.out.println() is used to print statement. Here, System is a class, out is the object of PrintStream class, println() is the method of PrintStream class.
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+	}
+}
+
+class A {
+}
+
+class B {
+}
+
+class C {
+}
