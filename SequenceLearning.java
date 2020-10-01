f216b1ec84958998f9c0db75891377abe89ae15b
kalpesh-mali <kalpesh.mali@bridgelabz.com>
2020-10-02 04:27:00 +0530

    SequenceLearning

---
+++ SequenceLearning.java
@@ -0,0 +1,10 @@
+package com.codedictator.learning;
+
+public class SequenceLearning {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		System.out.println("Sum = " + c);
+	}
+}
